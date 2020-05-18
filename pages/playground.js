d7cc530ed70eaac33a4b1343df2ffc094afba352
Cameron Ratliff <cratliff@colgate.edu>
2020-05-18 14:54:32 -0400

    added buttons to playground, still need functionality

--- pages/playground.js
+++ pages/playground.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { Controlled as CodeMirror } from 'react-codemirror2'
-import prettier from 'prettier-standalone'
+// import prettier from 'prettier-standalone'
 import Button from '@material-ui/core/Button'
 import Layout from '../components/Layout'
-// import { testClass, hookifyApp } from './utils'
+import { testClass, hookifyApp } from '../componentUtils'
 
 let modeLoaded = false
 if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
@@ -17,7 +17,8 @@ export default function Playground() {
 
   function handleSubmit() {
     setOutputCode(inputCode)
-    setOutputCode(prettierTest(inputCode))
+    // setOutputCode(prettierTest(inputCode))
+    setOutputCode(hookifyApp(inputCode))
   }
   let options = {
     lineNumbers: true,
@@ -39,89 +40,57 @@ export default function Playground() {
   }
   return (
     <Layout>
-      <div className="container-0">
-        <div className="container-1">
-          <div className="container-1a">
-            <p className="playground-header">Class Component</p>
-            <CodeMirror
-              value={inputCode}
-              options={options}
-              onBeforeChange={(editor, data, value) => {
-                setInputCode(value)
-              }}
-              onChange={(editor, data, value) => {
-                setInputCode(value)
-              }}
-            />
+      {modeLoaded && (
+        <div className="container-0">
+          <div className="container-1">
+            <div className="container-1a">
+              <p className="playground-header">Class Component</p>
+              <CodeMirror
+                value={inputCode}
+                options={options}
+                onBeforeChange={(editor, data, value) => {
+                  setInputCode(value)
+                }}
+                onChange={(editor, data, value) => {
+                  setInputCode(value)
+                }}
+              />
+              <div className="button-container">
+                <Button
+                  variant="contained"
+                  color="default"
+                  className="playground-button"
+                >
+                  copy
+                </Button>
+                <Button
+                  variant="contained"
+                  color="default"
+                  className="playground-button"
+                >
+                  clear
+                </Button>
+              </div>
+            </div>
+            <div className="container-1b">
+              <p className="playground-header">Hookfied Functional Component</p>
+              <CodeMirror value={outputCode} options={options2} />
+              <div className="button-container">
+                <Button
+                  variant="contained"
+                  color="default"
+                  className="playground-button"
+                >
+                  copy
+                </Button>
+              </div>
+            </div>
           </div>
-          <div className="container-1b">
-            <p className="playground-header">Hookfied Functional Component</p>
-            <CodeMirror value={outputCode} options={options2} />
-          </div>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
         </div>
-        <Button variant="contained" color="primary" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </div>
+      )}
     </Layout>
   )
 }
-
-export const testClass = `import React, { Component } from 'react'
-export default class TestFile1 extends Component {
-  constructor() {
-    super()
-    this.state = {
-      firstName: 'Bob',
-      lastName: 'Smith',
-    }
-  }
-  render() {
-    return (
-      <div>
-        <div>
-          Hello {this.state.firstName} {this.state.lastName}!
-        </div>
-        <button
-          type="button"
-          onClick={() => {
-            this.setState({
-              firstName: 'Tom',
-              lastName: 'Hanks',
-            })
-          }}
-        >
-          Change State!
-        </button>
-      </div>
-    )
-  }
-}`
-
-function prettierTest(string) {
-  let formattedTest = prettier.format(string, {
-    semi: false,
-    parser: 'babel',
-  })
-  return formattedTest
-}
-
-// export function hookifyApp(testStr) {
-//   let formattedTest = prettier.format(testStr, {
-//     semi: false,
-//     parser: 'babel',
-//   })
-//   // remove all comments
-//   formattedTest = formattedTest
-//     .replace(/(\/\/)(.*?)(\n)/g, '')
-//     .replace(/(\/\*)(.|\n)*?(\*\/)/g, '')
-//   const lifecycleCheck = /(componentDidMount\()|(componentDidUpdate\()|(componentWillUnmount\()/.test(
-//     formattedTest
-//   )
-//   const translatedFile = translateToFunctionComp(formattedTest)
-//   const formattedFinal = prettier.format(translatedFile, {
-//     semi: false,
-//     parser: 'babel',
-//   })
-//   return formattedFinal
-// }
