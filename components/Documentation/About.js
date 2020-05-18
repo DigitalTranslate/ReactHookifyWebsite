b3ac6a5e1203e001aa69dbfd5eda0c5fb2e6a85b
Bailey McKelway <bjlmckelway@gmail.com>
2020-05-18 16:34:18 -0400

    Documentation Layout

--- components/Documentation/About.js
+++ components/Documentation/About.js
@@ -4,9 +4,37 @@ export default function About() {
       <h2>About</h2>
       <div id="about_hookify">
         <h4>What is Hookify?</h4>
+        <p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et
+            tincidunt purus. Curabitur tincidunt enim nec nisi accumsan
+            ullamcorper. Maecenas at massa vel sapien feugiat eleifend in non
+            massa. Maecenas tincidunt libero vel orci dapibus posuere.
+            Suspendisse fringilla vulputate ante non condimentum. Proin lectus
+            magna, luctus quis lectus id, ornare condimentum nisi. Suspendisse
+            efficitur turpis erat, vitae placerat mauris luctus vitae.
+            Suspendisse sit amet felis at lacus aliquet fringilla accumsan et
+            enim. Sed a ex nec nibh hendrerit gravida.
+          </p>
+        </p>
+        <hr />
       </div>
       <div id="about_hooks">
         <h4>Why Use Hooks?</h4>
+        <p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et
+            tincidunt purus. Curabitur tincidunt enim nec nisi accumsan
+            ullamcorper. Maecenas at massa vel sapien feugiat eleifend in non
+            massa. Maecenas tincidunt libero vel orci dapibus posuere.
+            Suspendisse fringilla vulputate ante non condimentum. Proin lectus
+            magna, luctus quis lectus id, ornare condimentum nisi. Suspendisse
+            efficitur turpis erat, vitae placerat mauris luctus vitae.
+            Suspendisse sit amet felis at lacus aliquet fringilla accumsan et
+            enim. Sed a ex nec nibh hendrerit gravida.
+          </p>
+        </p>
+        <hr />
       </div>
     </div>
   );
