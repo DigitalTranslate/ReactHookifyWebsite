22aebc6f6e56aa34bbf2512737c8e99dd7508549
Cameron Ratliff <cratliff@colgate.edu>
2020-05-20 12:30:43 -0400

    updates

--- components/Documentation/Installation.js
+++ components/Documentation/Installation.js
@@ -5,12 +5,19 @@ export default function Installation() {
   return (
     <div id="installation">
       <h2>Installation</h2>
+      <div>
+        Current Version: <b>1.0.12</b>
+      </div>
       <div id="install_example">
         <Container maxWidth="sm">
           <p>
             Install with npm: <br />
           </p>
           <div className="grey">npm install -g react-hookify</div>
+          <p>
+            To update to a more recent version (if previously installed): <br />
+          </p>
+          <div className="grey">npm update -g react-hookify</div>
         </Container>
       </div>
     </div>
