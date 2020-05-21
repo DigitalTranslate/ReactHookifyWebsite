2211b582bdb7259a704723f04594bd53a39965d8
Cameron Ratliff <cratliff@colgate.edu>
2020-05-21 19:21:40 -0400

    adding more limitations

--- components/Documentation/examples/LimitationExamples.js
+++ components/Documentation/examples/LimitationExamples.js
@@ -18,28 +18,28 @@ export default function LimitationExamples() {
           keywords.
         </li>
         <li className="li-limits">
+          React Hookify cannot currently translate smiley/frowney faces such as
+          :) or :( (sorry!)
+        </li>
+        <li className="li-limits">
+          React Hookify does not support replacing the whole state with a new
+          object. There is not an exact hook equivalent. For example, replacing
+          the whole state using{' '}
+          <span className="code-span">this.setState(newObject)</span> will not
+          be able to be translated
+        </li>
+        <li className="li-limits">
           <b>
             React Hookify will not be able to translate instances where variable
             names do not actually exist on the current file.
           </b>{' '}
           The package is built with parsing logic that looks for variable
-          patterns. This may come up when replacing the whole state with{' '}
-          <span className="code-span">this.setState</span> and when using{' '}
-          <b>controlled forms</b>.
+          patterns. This may come up when using <b>controlled forms</b>.
           <ul>
             <li className="li-limits">
-              If you try to replace the whole state using{' '}
-              <span className="code-span">
-                this.setState(this.props.newObject)
-              </span>
-              , React Hookify would not have access to the needed keys/values
-              names that exist on newObject.
-            </li>
-            <li className="li-limits">
-              Similarly, if you try to implement a controlled form, but the
-              actual form component is written in a different file, React
-              Hookify again would not be able to identify the needed form names
-              and values.
+              If you try to implement a controlled form, but the actual form
+              component is in a different file, React Hookify would not be able
+              to identify the needed form names and values.
             </li>
           </ul>
         </li>
